f4b236c7814b5e168af4907d0ae244567f40caa1
Bdamir98 <amirshohel870@gmail.com>
2023-02-24 14:32:09 +0600

    update

--- lib/views/widgets/loading_widget.dart
+++ lib/views/widgets/loading_widget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:home_land/utils/constant.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 
+
 showLoadingDialog(BuildContext context) {
   showCupertinoDialog(
     context: context,
